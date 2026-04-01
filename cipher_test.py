5c3304a9d8e3cdd7105fcd979ad68a10c8f77c04
Abhiyan-smk <abhiyan.simkhada@cqumail.com>
2026-04-02 02:17:12 +1000

    Enhance cipher_test.py with decryption examples
    Added known key decryption and brute force attack examples for Caesar cipher.

--- cipher_test.py
+++ cipher_test.py
@@ -9,3 +9,28 @@ decrypted = cipher.decipher(encrypted)
 print("Plaintext:", plaintext)
 print("Encrypted:", encrypted)
 print("Decrypted:", decrypted)
+
+--- known key decryptiopo E.g---
+
+print("\nKnown Key Decryption:")
+print("Ciphertext: pjhigpaxp")
+print("Decrypted:", Caesar(15).decipher("pjhigpaxp")
+
+
+      --- Unknown Key (Brute Force E.g) ---
+
+print("\nBrute Force Attack on 'KHOORZRUOG':")
+
+cipher_text = "KHOORZRUOG"
+
+for i in range(1, 26):
+    print(i, Caesar(i).decipher(cipher_text))
+
+--- Unknown Key (Brute Force E.g) ---
+
+print("\nBrute Force Attack on 'KHOORZRUOG':")
+
+cipher_text = "KHOORZRUOG"
+
+for i in range(1, 26):
+    print(i, Caesar(i).decipher(cipher_text))
